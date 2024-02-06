f4d1e0d4adbb385b60bfbec1c565a87cbbc51e87
aliqadeer <aliqadeer9233@gmail.com>
2024-02-06 20:18:08 +0500

    add all pages + add react-router-dom + change name and  icon of app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Profie from './pages/Profie'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound';
+
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+     <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/profie" element={<Profie />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+    </>
   )
 }
 
