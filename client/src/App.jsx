4705c2eff9a1c146590fce5a6500027e00b43324
aliqadeer <aliqadeer9233@gmail.com>
2024-02-14 00:31:02 +0500

    Hreader completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
-import Profie from './pages/Profie'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound';
+import Header from './componants/Header';
+import Profile from './pages/Profile';
 
 
 function App() {
@@ -12,11 +13,12 @@ function App() {
   return (
     <>
      <BrowserRouter >
+     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/profie" element={<Profie />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
